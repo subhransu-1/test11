395af0bb4b7e500927a01cefde522ac6463c54bc
subhransu <100188157+subhransu-1@users.noreply.github.com>
2023-09-04 17:38:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t3.micro"
-  subnet_id     = "subnet-01ac37f7199e021c8"
-}
